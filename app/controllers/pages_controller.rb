8cbe8e0e602a044720d18753a89d54f03d82b47d
maurabaptista <maura.mendes.baptista@gmail.com>
2018-12-29 18:03:00 +0000

    nabbar, banner and calculator form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-  end
+  	@currencies = [["EUR", 1], ["USD", 0.873945], ["GBP", 1.10979], ["CAD", 0.641066], ["CHF", 0.887800]]
+  	@selected_currency = params[:currency]
+  	@container_size = params[:container].to_i
+  	@selected_volume = params[:volume].to_i
+  	@selected_metrics = params[:metrics]
+
+  	left_space = @container_size - @selected_volume
+  
+
+  	 end
 end
