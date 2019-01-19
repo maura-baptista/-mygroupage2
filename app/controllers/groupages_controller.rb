22118e337dd526242b038b42eab27c634c6848c4
maurabaptista <maura.mendes.baptista@gmail.com>
2019-01-19 17:18:52 +0000

    user sign in of groupages new action

--- app/controllers/groupages_controller.rb
+++ app/controllers/groupages_controller.rb
@@ -1,5 +1,5 @@
 class GroupagesController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:index, :show, :new]
+	skip_before_action :authenticate_user!, only: [:index, :show]
   # before_action :set_container, only: [:create]
   
   layout "calculator"
