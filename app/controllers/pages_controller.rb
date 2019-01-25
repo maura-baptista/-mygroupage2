31e1df5e50051810d667430365e2e65f897eff3e
maurabaptista <maura.mendes.baptista@gmail.com>
2019-01-25 12:00:08 +0000

    login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :login]
 
-  layout :resolve_layout
+  layout "devise", only: [:login]
+  # layout :resolve_layout
 
   def home
 
@@ -9,17 +10,19 @@ class PagesController < ApplicationController
 
   def login
   	@user = User.new
+  	# render layout: "devise"
+  	# render layout: false
   	
   end
 
   private
 
-  def resolve_layout
-  	case display
-    when "login"
-       "devise"
-    else
-       "application"
-    end
-  end
+  # def resolve_layout
+  # 	case display
+  #   when "login"
+  #      "devise"
+  #   else
+  #      "application"
+  #   end
+  # end
 end
