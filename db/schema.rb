adbd23a9dc450f1148cb40e9e717d6b21332be3b
maurabaptista <maura.mendes.baptista@gmail.com>
2019-02-14 11:25:23 +0000

    droped database

--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,6 @@ ActiveRecord::Schema.define(version: 2019_02_07_113340) do
     t.string "currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_containers_on_user_id"
   end
 
   create_table "flats", force: :cascade do |t|
@@ -92,15 +90,6 @@ ActiveRecord::Schema.define(version: 2019_02_07_113340) do
     t.float "lead_destination_long"
   end
 
-  create_table "user_savings", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "saving_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["saving_id"], name: "index_user_savings_on_saving_id"
-    t.index ["user_id"], name: "index_user_savings_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -123,9 +112,6 @@ ActiveRecord::Schema.define(version: 2019_02_07_113340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "containers", "users"
   add_foreign_key "groupages", "containers"
   add_foreign_key "groupages", "users"
-  add_foreign_key "user_savings", "containers", column: "saving_id"
-  add_foreign_key "user_savings", "users"
 end
